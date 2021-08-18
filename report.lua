6fa06a8a020eb5949fc5546a372c8c10a776b797
random <66913721+catvskid@users.noreply.github.com>
2021-08-18 16:15:38 -0300

    Update report.lua

--- report.lua
+++ report.lua
@@ -1,1 +1,4 @@
+--[ u can change reason and type of abuse, very simple script
+-- ex: game.Players:ReportAbuse(game.Players.LocalPlayer,"Type of abuse", "reason")
+
 game.Players:ReportAbuse(game.Players.LocalPlayer,"Swearing", "dating")
