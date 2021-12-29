27431fe4c02de532d4cb0c72f82a989d67af10a4
conwwy <66913721+lucciouss@users.noreply.github.com>
2021-12-28 21:26:43 -0300

    Update report.lua

--- report.lua
+++ report.lua
@@ -1,4 +1,4 @@
 --[ u can change reason and type of abuse, very simple script
 -- ex: game.Players:ReportAbuse(game.Players.LocalPlayer,"Type of abuse", "reason")
 
-game.Players:ReportAbuse(game.Players.LocalPlayer,"Swearing", "dating")
+game.Players:ReportAbuse(game.Players,"Swearing", "bypassing slurs and harassing me")
